2068b23ce7ceb4bd14a53859295ad51c83abbd6a
Adeolu Adeyemi <adestack@sholaadeyemi.com>
2019-08-06 23:48:18 +0100

    done

--- adestackbakery/settings.py
+++ adestackbakery/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'disbursement',
-    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +115,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
